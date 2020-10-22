5e19bc85766b63e1fd2be04000414e91b4668a7b
최윤주 <yeomju311@gmail.com>
2020-10-22 12:52:49 +0900

    state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,13 +6,28 @@ import './App.css';
 
 // class type
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // 상위 컴포넌트 App의 값을 하위 컴포넌트 Subject에 넘기는 것은 가능하다
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents: [
+        {id: 1, title: 'HTML', desc: 'HTML is HyperText ...'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         {/* Hello, React!! */}
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
