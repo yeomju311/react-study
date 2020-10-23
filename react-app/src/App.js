e4d8476b7283bcf26bb0c2bbd87d1f8d62772989
최윤주 <yeomju311@gmail.com>
2020-10-24 01:23:31 +0900

    delete 구현

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.max_content_id = 3;
     // 상위 컴포넌트 App의 값을 하위 컴포넌트 Subject에 넘기는 것은 가능하다
     this.state = {
-      mode: 'create',
+      mode: 'welcome',
       selected_content_id: 2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome: {title: 'Welcome', desc: 'Hello, React!!!'},
@@ -126,9 +126,28 @@ class App extends Component {
           }.bind(this)} data={this.state.contents}
         ></TOC>
         <Control onChangeMode={function(_mode) {
-          this.setState({
-            mode: _mode
-          });
+          if(_mode === 'delete') {
+            if (window.confirm('really?')) {
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while (i < _contents.length) {
+                if (_contents[i].id === this.state.selected_content_id) {
+                  _contents.splice(i, 1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode: 'welcome',
+                contents: _contents
+              });
+              alert('deleted');
+            }
+          } else {
+            this.setState({
+              mode: _mode
+            });
+          }
         }.bind(this)}></Control>
         {this.getContent()}
       </div>
