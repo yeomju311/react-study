14d08f7f7dd90d5db7c17dc872a5d63057e5cad0
최윤주 <yeomju311@gmail.com>
2020-10-23 23:06:14 +0900

    create 구현

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import Subject from './components/Subject';
+import Control from './components/Control';
 import './App.css';
 
 // class type
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     // 상위 컴포넌트 App의 값을 하위 컴포넌트 Subject에 넘기는 것은 가능하다
     this.state = {
-      mode: 'read',
+      mode: 'create',
       selected_content_id: 2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome: {title: 'Welcome', desc: 'Hello, React!!!'},
@@ -23,10 +26,11 @@ class App extends Component {
   }
   render() {
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'read') {
       var i = 0;
       while(i < this.state.contents.length) {
@@ -38,6 +42,31 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if (this.state.mode == 'create') {
+      _article = <CreateContent onSubmit={function(_title, _desc) {
+        // add Content to this.state.contents
+        this.max_content_id = this.max_content_id + 1;
+        // array.push의 경우 나중에 퍼포먼스 측면에서 문제가 생길 수 있다
+        // this.state.contents.push(
+        //   {id: this.max_content_id, title: _title, desc: _desc}
+        // );
+        var _contents = this.state.contents.concat(
+          {id: this.max_content_id, title: _title, desc: _desc}
+        );
+        // 배열의 복사 Array.from
+        // 객체의 복사, 추가 Object.assign
+        // var newContents = Array.from(this.state.contents);
+        // newContents.push(
+        //   {id: this.max_content_id, title: _title, desc: _desc}
+        // );
+        this.setState({
+          // content: this.state.contents,
+          contents: _contents,
+          // contents: newContents
+        });
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
     }
     console.log('render', this);
     return (
@@ -71,7 +100,12 @@ class App extends Component {
             });
           }.bind(this)} data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function(_mode) {
+          this.setState({
+            mode: _mode
+          });
+        }.bind(this)}></Control>
+        {_article}
       </div>
     );
   }
