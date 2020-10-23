99ee45472d389aba7d6c322f52251f5899f54a6b
최윤주 <yeomju311@gmail.com>
2020-10-24 01:15:39 +0900

    update 구현

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TOC from './components/TOC';
 import ReadContent from './components/ReadContent';
 import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import Subject from './components/Subject';
 import Control from './components/Control';
 import './App.css';
@@ -24,25 +25,26 @@ class App extends Component {
       ]
     }
   }
-  render() {
-    console.log('App render');
+  getReadContent() {
+    var i = 0;
+    while(i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if (data.id === this.state.selected_content_id) {
+        return data;
+        break;
+      }
+      i = i + 1;
+    }
+  }
+  getContent() {
     var _title, _desc, _article = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'read') {
-      var i = 0;
-      while(i < this.state.contents.length) {
-        var data = this.state.contents[i];
-        if (data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     } else if (this.state.mode == 'create') {
       _article = <CreateContent onSubmit={function(_title, _desc) {
         // add Content to this.state.contents
@@ -64,11 +66,34 @@ class App extends Component {
           // content: this.state.contents,
           contents: _contents,
           // contents: newContents
+          mode: 'read',
+          selected_content_id: this.max_content_id,
         });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
+    } else if (this.state.mode === 'update') {
+      var _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_id, _title, _desc) {
+        var _contents = Array.from(this.state.contents);
+        var i = 0;
+        while(i < _contents.length) {
+          if (_contents[i].id === _id){
+            _contents[i] = {id: _id, title: _title, desc: _desc};
+            break;
+          }
+          i = i + 1;
+        }
+        this.setState({
+          contents: _contents,
+          mode: 'read'
+        });
+        console.log(_title, _desc);
+      }.bind(this)}></UpdateContent>
     }
-    console.log('render', this);
+    return _article;
+  }
+  render() {
+    console.log('App render');
     return (
       <div className="App">
         {/* Hello, React!! */}
@@ -105,7 +130,7 @@ class App extends Component {
             mode: _mode
           });
         }.bind(this)}></Control>
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
